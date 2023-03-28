b980acf66cf0f262f6b5a782ef5f94fa50cddf42
etblink <veg4ssnow@gmail.com>
2023-03-28 16:53:29 -0700

    Convert Markdown to HTML in blog posts

--- src/BlogPostPage.js
+++ src/BlogPostPage.js
@@ -3,7 +3,7 @@ import { getPost } from './hive';
 import { useParams } from 'react-router-dom';
 
 function BlogPostPage({ match }) {
-    const { author, permlink } = useParams();
+  const { author, permlink } = useParams();
   const [post, setPost] = useState(null);
 
   useEffect(() => {
@@ -15,13 +15,41 @@ function BlogPostPage({ match }) {
     fetchData();
   }, [author, permlink]);
 
+  const renderPostContent = () => {
+    const converter = new showdown.Converter();
+    const htmlContent = converter.makeHtml(post.body);
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlContent, 'text/html');
+
+    const baseUrl = 'https://images.hive.blog/';
+
+    const images = doc.querySelectorAll('img');
+    images.forEach((img) => {
+      const imgUrl = img.getAttribute('src');
+      if (!imgUrl.startsWith('http')) {
+        img.setAttribute('src', baseUrl + imgUrl);
+      }
+    });
+
+    const links = doc.querySelectorAll('a');
+    links.forEach((link) => {
+      const linkUrl = link.getAttribute('href');
+      if (!linkUrl.startsWith('http')) {
+        link.setAttribute('href', baseUrl + linkUrl);
+      }
+    });
+
+    return { __html: doc.body.innerHTML };
+  };
+
   return (
     <div>
       {post ? (
         <>
           <h1>{post.title}</h1>
           <p>By {post.author}</p>
-          <div dangerouslySetInnerHTML={{ __html: post.body }} />
+          <div dangerouslySetInnerHTML={renderPostContent()} />
         </>
       ) : (
         <p>Loading...</p>
