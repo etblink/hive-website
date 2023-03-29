71c15044ba48b45d46efdff8cb11273296497586
etblink <veg4ssnow@gmail.com>
2023-03-28 19:58:26 -0700

    cleaned up unused variable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
