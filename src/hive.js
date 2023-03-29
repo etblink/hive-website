416c3d0cedaba8d8bd882d6c396bb66c1da2acb1
etblink <veg4ssnow@gmail.com>
2023-03-29 15:50:25 -0700

    refactored hivejs

--- src/hive.js
+++ src/hive.js
@@ -1,7 +1,9 @@
-import { Client, DatabaseAPI } from '@hiveio/dhive';
+import { Client, DatabaseAPI } from "@hiveio/dhive";
 
 // Initialize the Hive client
-const client = new Client('https://api.hive.blog');
+const client = new Client("https://api.hive.blog");
+
+const db = new DatabaseAPI(client);
 
 // Function to fetch user account details
 export async function getUserAccount(username) {
@@ -9,39 +11,37 @@ export async function getUserAccount(username) {
     const accounts = await client.database.getAccounts([username]);
     return accounts.length > 0 ? accounts[0] : null;
   } catch (error) {
-    console.error('Error fetching user account:', error);
+    console.error("Error fetching user account:", error);
     return null;
   }
 }
 
-export async function fetchRecent3Posts(username, limit = 9) {
-  const db = new DatabaseAPI(client);
+async function fetchPosts(username, limit, discussionType = "blog") {
   try {
-    const posts = await db.getDiscussions('blog', { tag: username, limit });
+    const posts = await db.getDiscussions(discussionType, {
+      tag: username,
+      limit,
+    });
     return posts;
   } catch (error) {
-    console.error('Error fetching blog posts:', error);
+    console.error(`Error fetching ${discussionType} posts:`, error);
     return [];
   }
 }
 
+export function fetchRecent3Posts(username, limit = 9) {
+  return fetchPosts(username, limit);
+}
 
-export async function fetchBlogPosts(username, limit = 20) {
-  const db = new DatabaseAPI(client);
-  try {
-    const posts = await db.getDiscussions('blog', { tag: username, limit });
-    return posts;
-  } catch (error) {
-    console.error('Error fetching blog posts:', error);
-    return [];
-  }
+export function fetchBlogPosts(username, limit = 20) {
+  return fetchPosts(username, limit);
 }
 
 export async function getPost(author, permlink) {
   try {
-    const post = await client.database.call('get_content', [author, permlink]);
+    const post = await client.database.call("get_content", [author, permlink]);
     return post;
   } catch (error) {
-    console.error('Error fetching post:', error);
+    console.error("Error fetching post:", error);
   }
 }
