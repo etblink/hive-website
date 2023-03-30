eef0a59971a43df0f117356d55d0970413380b1f
etblink <veg4ssnow@gmail.com>
2023-03-30 11:59:42 -0700

    comments added to code

--- src/utils/getFirstImageUrl.js
+++ src/utils/getFirstImageUrl.js
@@ -1,7 +1,10 @@
+// Function to extract the first image URL from a markdown content string
 function getFirstImageUrl(content) {
+  // Regular expression to match the markdown image syntax
   const imgRegex = /!\[[^\]]*\]\(([^)]+)\)/;
   const match = imgRegex.exec(content);
 
+  // Extract the first image URL from the content string and return it
   if (match && match[1]) {
     return match[1];
   }
