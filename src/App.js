34fac3e7ba77d16df60a892c99808e3282ccd662
etblink <veg4ssnow@gmail.com>
2023-03-30 08:36:18 -0700

    styled background of dropdown menu link

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
 
   return (
     <div className="app">
-      <h1 className="app__title">Welcome to ETBlink's Blog</h1>
-      <BrowserRouter>
+        <h1 className="app__title">Welcome to ETBlink's Blog</h1>
+        <BrowserRouter>
         <nav className="app__nav">
           <Link to="/">Home</Link>
           <span> | </span>
@@ -21,39 +21,39 @@ function App() {
               Recommended Bloggers
             </Link>
             <div className="dropdown-content">
-              <Link to="/blogger/edicted">edicted</Link>
-              <Link to="/blogger/theycallmedan">theycallmedan</Link>
-              <Link to="/blogger/taskmaster4450">taskmaster4450</Link>
-              <Link to="/blogger/khaleelkazi">khaleelkazi</Link>
+              <Link to="/blogger/edicted dropdown-link">edicted</Link>
+              <Link to="/blogger/theycallmedan dropdown-link">theycallmedan</Link>
+              <Link to="/blogger/taskmaster4450 dropdown-link">taskmaster4450</Link>
+              <Link to="/blogger/khaleelkazi dropdown-link">khaleelkazi</Link>
             </div>
           </div>
         </nav>
-        <div className="app__container">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <div className="posts-container app__posts-container">
-                    <div className="post-items app__post-items">
-                      {recentPosts.map((post, index) => (
-                        <BlogPost
-                          key={post.permlink}
-                          post={post}
-                          index={index}
-                        />
-                      ))}
-                    </div>
+      <div className="app__container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="posts-container app__posts-container">
+                  <div className="post-items app__post-items">
+                    {recentPosts.map((post, index) => (
+                      <BlogPost
+                        key={post.permlink}
+                        post={post}
+                        index={index}
+                      />
+                    ))}
                   </div>
-                </>
-              }
-            />
-            <Route path="/post/:author/:permlink" element={<BlogPostPage />} />
-            <Route
-              path="/blogger/:username"
-              element={<BloggerPage />}
-            />
-          </Routes>
+                </div>
+              </>
+            }
+          />
+          <Route path="/post/:author/:permlink" element={<BlogPostPage />} />
+          <Route
+            path="/blogger/:username"
+            element={<BloggerPage />}
+          />
+        </Routes>
         </div>
       </BrowserRouter>
       <div class="hive-icon">
