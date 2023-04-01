33ed76c61d9200b3095250679b22bef6e38c8735
etblink <veg4ssnow@gmail.com>
2023-04-01 10:55:25 -0700

    Blogger Profile Styling

--- src/components/BloggerProfile.js
+++ src/components/BloggerProfile.js
@@ -22,13 +22,13 @@ function BloggerProfile() {
   // Extract the profile image from the posting_json_metadata
   const postingMetadata = JSON.parse(account.posting_json_metadata);
   console.log("Posting metadata:", postingMetadata); // Debugging
-  const profileImage = postingMetadata.profile.profile_image || "https://via.placeholder.com/150";
+  const profileImage = postingMetadata.profile.profile_image || "https://developers.hive.io/images/sticker.png";
 
   return (
     <div className="blogger-profile">
       <img className="blogger-profile__image" src={profileImage} alt={`${username}'s profile`} />
       <h2>About this Author</h2>
-      <p>Section to be filled in later.</p>
+      <p>This section is to be filled in later. Eventually I will set up the OpenAI API to connect with GPT. My plan is to fetch the Hive users 5 most recent blog posts and extract the author's interests from each using GPT. Then I will use that information to interact with GPT a second time and have it write a short bio for the blogger. At some point, this text will be replaced with AI generated text.</p>
     </div>
   );
 }
